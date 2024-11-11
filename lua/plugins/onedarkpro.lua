03d0b42df4ae26a7abeced5a292bf92a5140d701
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-11-11 16:56:42 +0100

    Remove custom highlights for CursorLine and CursorLineNr in onedarkpro configuration

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -5,10 +5,6 @@ require("onedarkpro").setup({
         terminal_colors = true, -- Apply theme to Neovim terminal
         cursorline = true, -- Highlight the line under the cursor
     },
-    highlights = {
-        CursorLine = { bg = "#2C323C" }, -- Changes the background color of the selected line
-        CursorLineNr = { fg = "#FFD700", bold = true }, -- Changes the color of the selected line number
-    },
 })
 
 -- Activate the theme
