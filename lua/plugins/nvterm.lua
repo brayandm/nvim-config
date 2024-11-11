5f90d0b8084380f604a8813421a2b6c1ee40127b
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-11-11 16:26:29 +0100

    Refactor nvterm setup and configuration for improved terminal management

--- lua/plugins/nvterm.lua
+++ lua/plugins/nvterm.lua
@@ -1,22 +1,18 @@
-require("nvterm").setup({
+require("nvterm").setup {
     terminals = {
-        shell = vim.o.shell, -- Use the default shell of the system
-        list = {}, -- Must be initialized as an empty table
-        type_opts = {
-            float = {
-                relative = "editor",
-                row = 0.3,
-                col = 0.3,
-                width = 0.4,
-                height = 0.4,
-                border = "rounded",
-            },
-            horizontal = { location = "rightbelow", split_ratio = 0.3 },
-            vertical = { location = "rightbelow", split_ratio = 0.5 },
+      shell = vim.o.shell, 
+      list = {},
+      type_opts = {
+        float = {
+          relative = "editor",
+          row = 0.1,
+          col = 0.1,
+          width = 0.8,
+          height = 0.8,
+          border = "rounded",
         },
+        horizontal = { location = "bottom", split_ratio = 0.3 },
+        vertical = { location = "right", split_ratio = 0.5 },
+      },
     },
-    behavior = {
-        autoclose_on_quit = true, -- Automatically close terminal on Neovim quit
-        close_on_exit = true,     -- Automatically close terminal when the process exits
-    },
-})
+  }
