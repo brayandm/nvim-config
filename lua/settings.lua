8ee88b074f2db3dc1436acf039eb9457a6de2eb8
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-11-11 16:37:33 +0100

    Set leader key to space in settings.lua and adjust clipboard configuration

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,3 +1,8 @@
+-- Map leader key to space
+-- Must be set before plugins are loaded
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- General Neovim settings
 vim.opt.number = true                -- Show line numbers
 vim.opt.relativenumber = true        -- Relative line numbers
@@ -6,7 +11,3 @@ vim.opt.shiftwidth = 4               -- Indentation width
 vim.opt.tabstop = 4                  -- Tab width
 vim.opt.mouse = 'a'                  -- Enable mouse support
 vim.opt.clipboard = 'unnamedplus'    -- Use system clipboard
-
--- Map leader key to space
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
