c08ec3577969e90896f646d5255304b53f439b5e
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-11-11 17:17:14 +0100

    Add autocmd to set NvimTreeCursorLine highlight on ColorScheme change

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,4 +2,8 @@
 require('nvim-tree').setup()
 
 -- Set the NvimTree highlight group for the cursor line
-vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#2C323C" })
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function()
+        vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#2C323C" })
+    end,
+})
